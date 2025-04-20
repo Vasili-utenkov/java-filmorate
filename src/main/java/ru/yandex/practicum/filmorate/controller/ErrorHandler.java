a30fb3c5f7d86f2d793a85354a5e20ed14f1c85a
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-04-21 02:56:09 +0300

    версия 1 без эксепшенов

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,8 +1,20 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFoundException(final NotFoundException e) {
+        return new ErrorResponse(
+                "искомый объект не найден.",
+                "Текст ошибки: " + e.getMessage()
+        );
+    }
 }
