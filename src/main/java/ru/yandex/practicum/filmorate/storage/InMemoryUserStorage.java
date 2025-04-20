f2efabe95a62d3211d5f7acc8f04ee4d66475407
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-04-20 23:50:50 +0300

    версия 1 без эксепшенов

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
