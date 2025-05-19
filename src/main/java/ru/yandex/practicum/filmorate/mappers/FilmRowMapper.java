759cea498e61350766aceef4b19de50bbd7233ba
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-05-19 22:32:51 +0300

    Начало скрещивания ежа с ужом

--- src/main/java/ru/yandex/practicum/filmorate/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/FilmRowMapper.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.filmorate.mappers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class FilmRowMapper implements RowMapper<Film> {
     @Override
     public Film mapRow(ResultSet resultSet, int rowNum) throws SQLException {
