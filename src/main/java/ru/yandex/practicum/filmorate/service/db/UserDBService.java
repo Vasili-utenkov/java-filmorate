14bac2d224f8c837689744f8cd568b1a417e7c8a
Vasili Utenkov <vasili.utenkov@yandex.ru>
2025-05-20 23:49:09 +0300

    Тестим ужа в еже

--- src/main/java/ru/yandex/practicum/filmorate/service/db/UserDBService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/db/UserDBService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service.db;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
